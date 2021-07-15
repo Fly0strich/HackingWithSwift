67bb1256b2112c75f92d2b3ce01f943ae7952e94
Shae Willes <shae.willes@gmail.com>
2021-07-14 21:59:28 -0600

    Added accessibility features to Bookworm

--- SwiftUI/Bookworm/Bookworm/RatingView.swift
+++ SwiftUI/Bookworm/Bookworm/RatingView.swift
@@ -27,11 +27,14 @@ struct RatingView: View {
             }
             
             ForEach(1..<maximumRating + 1) { number in
-                self.image(for: number)
-                    .foregroundColor(number > self.rating ? self.offColor : self.onColor)
+                image(for: number)
+                    .foregroundColor(number > rating ? offColor : onColor)
                     .onTapGesture {
-                        self.rating = number
+                        rating = number
                     }
+                    .accessibility(label: Text("\(number == 1 ? "1 star" : "\(number) stars")"))
+                    .accessibility(removeTraits: .isImage)
+                    .accessibility(addTraits: number > rating ? .isButton : [.isButton, .isSelected])
             }
         }
     }
